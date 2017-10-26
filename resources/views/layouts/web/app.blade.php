f51a5b21da0aa614a83000d9669334e25db9e38a
Iustinian Monea <moneaiustin@gmail.com>
2017-10-26 19:11:55 +0300

    Deleted files

--- resources/views/layouts/web/app.blade.php
+++ resources/views/layouts/web/app.blade.php
@@ -15,7 +15,6 @@
 
         <!--Custom CSS-->
         <link href="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/css/toastr.min.css" rel="stylesheet">
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.47/js/bootstrap-datetimepicker.min.js"></script>
 
         <!--[if lt IE 9]>
           <script src="{{ URL::asset('js/vendor/html5shiv.min.js')}}"></script>
