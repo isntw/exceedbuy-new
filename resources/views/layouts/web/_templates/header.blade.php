72768d64e53b0eb2361ea94dba852470e72e4186
Iustinian Monea <moneaiustin@gmail.com>
2017-11-26 18:29:57 +0200

    Products Header + amazon link

--- resources/views/layouts/web/_templates/header.blade.php
+++ resources/views/layouts/web/_templates/header.blade.php
@@ -2,13 +2,13 @@
 
 <header role="banner" class="probootstrap-header">
     <div class="container">
-        <a href="/" class="probootstrap-logo"><img src="/img/logo.gif" height="60" alt="Logo" ></a>
+        <a href="/" class="probootstrap-logo"><img src="/img/logo.gif" height="60" alt="Logo"></a>
 
-        <a href="#" class="probootstrap-burger-menu visible-xs" ><i>Menu</i></a>
+        <a href="#" class="probootstrap-burger-menu visible-xs"><i>Menu</i></a>
         <div class="mobile-menu-overlay"></div>
-        <ul class="probootstrap-right-nav hidden-xs" id='info' >
-            <li ><i class="icon-phone" ></i>Call Us : +755 413 5223</li>
-            <li ><i class="icon-mail" ></i>Email Us: contact@exceedbuy.com</li>
+        <ul class="probootstrap-right-nav hidden-xs" id='info'>
+            <li><i class="icon-phone"></i>Call Us : +755 413 5223</li>
+            <li><i class="icon-mail"></i>Email Us: contact@exceedbuy.com</li>
         </ul>
         <nav role="navigation" class="navbar probootstrap-nav hidden-xs">
             <ul class="probootstrap-main-nav">
@@ -23,13 +23,15 @@
                 <li><a href="https://www.facebook.com/WeLoveGorgeousBabies/"><i class="icon-facebook2"></i></a></li>
                 <li><a href="#"><i class="icon-google-plus"></i></a></li>
 
-            </ul >
+            </ul>
 
 
             <div class="extra-text visible-xs">
                 <a href="#" class="probootstrap-burger-menu"><i>Menu</i></a>
-                <h5>Address</h5>
-                <p>198 West 21th Street, Suite 721 New York NY 10016</p>
+                <h5>Mail</h5>
+                <p>contact@exceedbuy.com</p>
+                <h5>Number</h5>
+                <p>	+755 413 5223</p>
                 <h5>Connect</h5>
                 <ul class="social-buttons">
                     <li><a href="#"><i class="icon-twitter"></i></a></li>
@@ -50,13 +52,14 @@
                         <div class="slides-text probootstrap-animate" data-animate-effect="fadeIn">
                             <h2>{{ $title or '' }}</h2>
                             <p style="font-size: 20px;">{{ $description or '' }}</p>
-                            @if(Route::currentRouteName() == 'web.index')
-                            <i class="fa fa-arrow-circle-down fa-4x probootstrap-animate" id='button' data-animate-effect="fadeIn"></i>
+                            @if(Route::currentRouteName() == 'web.index' or Route::currentRouteName() == 'web.product.ear')
+                                <i class="fa fa-arrow-circle-down fa-4x probootstrap-animate" id='button'
+                                   data-animate-effect="fadeIn"></i>
                             @endif
                             @if(Route::currentRouteName() == 'web.contest.index')
-                            <h2> 
-                                <div id="clock"></div> 
-                            </h2> 
+                                <h2>
+                                    <div id="clock"></div>
+                                </h2>
                             @endif
 
                         </div>
